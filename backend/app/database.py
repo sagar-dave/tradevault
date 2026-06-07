da4b5770d30f0b4596c39d5c74fc251bf4f51e24
Sagar Dave <sega@Mac.attlocal.net>
2026-06-07 13:18:45 -0500

    Use Kubernetes ConfigMap and Secret for database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,21 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-import os
-DATABASE_URL = os.getenv("DATABASE_URL")
-print(f"DB URL: {DATABASE_URL}")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
