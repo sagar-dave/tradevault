e119373b4036df3d41bf4fb2e3d9b630e90fd742
Sagar Dave <sega@Mac.attlocal.net>
2026-06-13 17:32:01 -0500

    Automate backend image build and

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from app.routes.trades import router as trades_router
 from app.database import engine, Base
 from app.models.trade_model import TradeModel
+from prometheus_fastapi_instrumentator import Instrumentator
 
 import socket
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(trades_router)
 
