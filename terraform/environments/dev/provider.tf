c07abf7c967ff2b4482b1e26a1069d183df997ca
Sagar Dave <sega@Mac.attlocal.net>
2026-06-17 23:03:23 -0500

    Migrate Terraform state to S3 backend

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+backend "s3" {
+    bucket = "tradevault-terraform-state-230470759616-us-east-2"
+    key = "tradevault/dev/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+    use_lockfile = true
+}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
