a4a1a16e4dcb4e5011bf254aa89b94649c565af0
Sagar Dave <sega@Mac.attlocal.net>
2026-06-21 18:23:38 -0500

    Allow ECR cleanup during dev destroy

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -153,6 +153,7 @@ resource "aws_security_group" "db" {
 resource "aws_ecr_repository" "api" {
   name                 = "${local.name_prefix}-api"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
